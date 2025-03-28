6757dc589117c9e3cbbdc5034c64b16be0ff5626
Martín Bocanegra <martin@uin.tech>
2025-03-28 16:39:14 -0300

    fix comments

--- tp1.py
+++ tp1.py
@@ -16,10 +16,10 @@ def calcular_intersecciones(timestamps_transferencias, timestamps_sospechoso):
     timestamps_transferencias.sort(key=lambda x: x[0]-x[1]) # O(n * log(n))
     for i in range(len(timestamps_transferencias)):
         if not hay_interseccion(timestamps_transferencias[i], timestamps_sospechoso[i]):
-            # No es sospechoso
+            # No es culpable
             return intersecciones
         intersecciones.append((timestamps_transferencias[i], timestamps_sospechoso[i]))
-    # Es sospechoso
+    # Es culpable
     return intersecciones
 
 def main():
